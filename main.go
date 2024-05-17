f03e80b5b381c36bd38d37630a4e2f699e6853e6
MahmoudFarouq <mahmoud.farouk987@gmail.com>
2024-05-17 19:27:01 +0400

    add dummy handler

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
+
+func itineraryHandler(c echo.Context) error {
+	var request [][]string
+	if err := c.Bind(&request); err != nil {
+		return err
+	}
+
+	return c.JSON(200, request)
+}
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.POST("/", itineraryHandler)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
